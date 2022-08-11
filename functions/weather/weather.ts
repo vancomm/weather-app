b4af84e9a790ed4204c42c3ec8c54a5567fa3b13
vancomm <vanushnick@gmail.com>
2022-08-11 12:07:36 +0300

    Added a weather function

---
+++ functions/weather/weather.ts
@@ -0,0 +1,36 @@
+import { Handler } from "@netlify/functions";
+
+export const handler: Handler = async (event, context) => {
+  const lat = event.queryStringParameters?.lat;
+  const lon = event.queryStringParameters?.lon;
+
+  if (!lat || !lon)
+    return {
+      statusCode: 422,
+      body: JSON.stringify({
+        message: "Request must include 'lat' and 'lon' parameters",
+      }),
+    };
+
+  const apiKey = process.env.OPENWEATHER_API_KEY;
+
+  const route = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}`;
+
+  const res = await fetch(route);
+
+  if (!res.ok) {
+    return {
+      statusCode: 400,
+      body: JSON.stringify({
+        message: "Could not fetch weather",
+      }),
+    };
+  }
+
+  const data = await res.json();
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify(data),
+  };
+};
